b3bc4290b3a5256473ab78db31cf38343fd85964
Tkumar02 <ta_265@yahoo.co.uk>
2023-10-17 17:57:46 +0100

    environment changes

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,
+    firebaseConfig: {
+        apiKey: "AIzaSyBZaNnqoTIr92oi1kJavOq8kItxNsdrwyw",
+        authDomain: "ta-word.firebaseapp.com",
+        projectId: "ta-word",
+        storageBucket: "ta-word.appspot.com",
+        messagingSenderId: "698297419500",
+        appId: "1:698297419500:web:1c4b23d089b837573f07c9"
+    }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'ta-word',
+    appId: '1:698297419500:web:1c4b23d089b837573f07c9',
+    storageBucket: 'ta-word.appspot.com',
+    apiKey: 'AIzaSyBZaNnqoTIr92oi1kJavOq8kItxNsdrwyw',
+    authDomain: 'ta-word.firebaseapp.com',
+    messagingSenderId: '698297419500',
+    measurementId: 'G-7EPPYR739L',
+  },};
