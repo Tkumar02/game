8637a7e448bf55f0bd8e3dd6443e4e0270e29172
Tkumar02 <ta_265@yahoo.co.uk>
2023-10-18 00:41:08 +0100

    firebase linked and changes made

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import { environment } from '../environments/environment.prod';
+import { AngularFireModule } from '@angular/fire/compat/';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore'
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CreateComponent } from './admin/create/create.component';
 import { HomeComponent } from './home/home.component';
 import { SimpleComponent } from './play/simple/simple.component';
+
+// import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+// import { environment } from '../environments/environment';
+// import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -20,7 +28,10 @@ import { SimpleComponent } from './play/simple/simple.component';
   imports: [
     BrowserModule,
     AppRoutingModule, 
-    FormsModule,
+    FormsModule, 
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
+    //provideFirebaseApp(() => initializeApp(environment.firebase)), provideFirestore(() => getFirestore()),
   ],
   providers: [],
   bootstrap: [AppComponent]
