5aa558f4304ae6ba92c9015fdd2ab153f0dee414
Tkumar02 <ta_265@yahoo.co.uk>
2023-10-23 03:05:35 +0100

    basic logic working to guess static word

--- src/app/play/simple/simple.component.ts
+++ src/app/play/simple/simple.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Form } from '@angular/forms';
 
 @Component({
   selector: 'app-simple',
@@ -6,49 +7,205 @@ import { Component } from '@angular/core';
   styleUrls: ['./simple.component.css']
 })
 export class SimpleComponent {
-  current: number = 0;
-  word: string = 'guess';
-  inputValues: Array<string> = ['','','','',''];
-  backgroundColour: Array<string> = ['white','white','white','white','white'];
-  letterStore: Array<string> = [];
+  chosenWord: string =  'guess';
+  finalWord: string = '';
+  wordArray: Array<string> = [];
+  guessedWord: string = '';
+  newArray: Array<string> = [];
+
+  box:string='';
+  attemptComplete: boolean=false;
+  num: number = 0;
+  check: boolean = false;
+  failed: boolean = false;
+  submitted: boolean = false;
+
+  modelBoxes1: string[] = ['','','','',''];
+  modelBoxes2: string[] = ['','','','',''];
+  modelBoxes3: string[] = ['','','','',''];
+  modelBoxes4: string[] = ['','','','',''];
+  modelBoxes5: string[] = ['','','','',''];
+
+  attempt1: boolean = false;
+  attempt2: boolean = false;
+  attempt3: boolean = false;
+  attempt4: boolean = false;
+
+  button1: boolean = false;
+  button2: boolean = false;
+  button3: boolean = false;
+  button4: boolean = false;
+  button5: boolean = false;
+
+  completed:boolean = false;
   
-  constructor() {}
+
+  constructor(){}
 
   ngOnInit(): void{
-    for(let i=0;i<5;i++){
-      this.letterStore.push(this.word.charAt(i))
+    
+  }
+
+  moveBox(event:Event, nextEvent:number, attempt:number){
+    switch(attempt){
+      case 1:
+        this.box='one'
+        this.num = 1
+        break;
+      case 2:
+        this.box='two'
+        this.num = 2;
+        break;
+      case 3:
+        this.box='three'
+        break;
+      case 4:
+        this.box='four'
+        break;
+      case 5:
+        this.box='five'
+        break;
+    }
+    //thisBox is the current box user is in
+    const thisBox = event.target as HTMLInputElement
+    //using id of next box to select that element
+    const nextBox = document.getElementById(nextEvent.toString() + this.box) as HTMLInputElement
+        
+    if(/^[a-zA-Z]+$/.test(thisBox.value) && parseInt(thisBox.id)<4){
+      nextBox.focus()
+      //console.log(thisBox.id)
+    } 
+  }
+
+  onSubmit(num:number){
+    this.check=false;
+  switch(num){
+      case 1:
+        this.wordArray = [...this.chosenWord]
+        this.box='one'
+        this.checkWord(this.box, this.modelBoxes1)
+        this.attempt1 = true;
+        this.button1 = true;        
+        break;
+      case 2:
+        this.wordArray = [...this.chosenWord]
+        this.newArray=[]
+        this.finalWord=''
+        this.box='two'
+        this.checkWord(this.box, this.modelBoxes2)
+        this.attempt2=true
+        this.button2 = true
+        break;
+      case 3:
+        this.wordArray = [...this.chosenWord]
+        this.newArray=[]
+        this.finalWord=''
+        this.box='three'
+        this.checkWord(this.box, this.modelBoxes3)
+        this.attempt3=true
+        this.button3=true
+        break;
+      case 4:
+        this.wordArray = [...this.chosenWord]
+        this.newArray=[]
+        this.finalWord=''
+        this.box='four'
+        this.checkWord(this.box, this.modelBoxes4)
+        this.attempt4=true
+        this.button4=true
+        break;
+      case 5:
+        this.wordArray = [...this.chosenWord]
+        this.newArray=[]
+        this.finalWord=''
+        this.box='five'
+        this.checkWord(this.box, this.modelBoxes5)
+        this.submitted= true
+        break;
     }
   }
 
-  moveBox(index: HTMLInputElement,key:any){
-    //console.log(key)
-    this.current = parseInt(index.id)
-    if(this.current<30 && key!='Backspace'){
-      const next = (this.current+1).toString()
-      document.getElementById(next)?.focus()
-    }
-    else if(key=='Backspace'){
-      const previous = (this.current-1).toString()
-      const previousBox = document.getElementById(previous)
-      previousBox?.focus()
-    }
-  }
+  // checkWord(box:string){
+  //   for(let i=0;i<5;i++){
+  //     const element = document.getElementById(i.toString()+box) as HTMLInputElement;
+  //     this.newArray.push(element.value);
+  //     this.finalWord=this.finalWord+element.value
+  //     if(this.newArray[i]==this.wordArray[i]){
+  //       element.style.backgroundColor = 'green';
+  //       this.wordArray[i]='xx';
+  //       console.log(this.wordArray,'green')
+  //     }
+  //     else if(this.wordArray.includes(element.value)){
+  //       element.style.backgroundColor = 'yellow';
+  //       console.log(this.wordArray, 'yellow')
+  //     }
+  //     else{
+  //       element.style.backgroundColor = 'red';
+  //     }
+  //   }
+  //   if(this.finalWord==this.chosenWord){
+  //     this.completed=true
+  //     alert('Successfully completed!')
+  //   }
+  // }
 
-  onSubmit(){
+  checkWord(box:string, modelboxes:any){
+    this.newArray = [...modelboxes]
+    this.finalWord = modelboxes.join('')
     for(let i=0;i<5;i++){
-      if(this.inputValues[i]==this.word.charAt(i)){
-        this.backgroundColour[i] = 'green';
-        const indexRemove = this.letterStore.indexOf(this.inputValues[i])
-        this.letterStore.splice(indexRemove,1)
-     }
+      const elementGreen = document.getElementById(i.toString()+box) as HTMLInputElement;
+      if(this.newArray[i]==this.wordArray[i]){
+        elementGreen.style.backgroundColor = 'green';
+        this.wordArray[i]='xx';
+        this.newArray[i]='xx';
+        console.log(this.wordArray,'green')
+      }
+      else{
+        console.log(i)
+      }
     }
     for(let i=0;i<5;i++){
-      if(this.letterStore.includes(this.inputValues[i])){
-        this.backgroundColour[i] = 'yellow';
-        const indexRemove = this.letterStore.indexOf(this.inputValues[i])
-        this.letterStore.splice(indexRemove,1)
+      const elementYellow = document.getElementById(i.toString()+box) as HTMLInputElement;
+      if(this.wordArray.includes(elementYellow.value)){
+        elementYellow.style.backgroundColor = 'yellow';
+        console.log('yellow',elementYellow.value )
+      }
+    }
+    for(let i=0;i<5;i++){
+      const elementRed = document.getElementById(i.toString()+box) as HTMLInputElement;
+      if(elementRed.style.backgroundColor != 'green' && elementRed.style.backgroundColor!='yellow'){
+        elementRed.style.backgroundColor = 'red';
       }
     }
     
+    if(this.finalWord==this.chosenWord){
+      this.completed=true
+      alert('Successfully completed!')
+    }
+  }
+
+  onInput(i:number, n:number){
+    switch(n){
+      case 1:
+        this.check = this.modelBoxes1.every(box => box.trim() !== '');
+        break;
+      case 2:
+        this.check = this.modelBoxes2.every(box => box.trim() !== '');
+        break;
+      case 3:
+        this.check = this.modelBoxes3.every(box => box.trim() !== '');
+        break;
+      case 4:
+        this.check = this.modelBoxes4.every(box => box.trim() !== '');
+        break;
+      case 5:
+        this.check = this.modelBoxes5.every(box => box.trim() !== '');
+        this.failed=true;
+        break;
+    }
+  }
+  
+  onSubmitForm(f:any){
+    console.log('HELLOO')
   }
 }
